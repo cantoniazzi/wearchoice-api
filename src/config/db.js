71c9d731f62266dd5162d867ceb8ad0ba50a07cc
Marciel Torres <marcielribeirotorres@gmail.com>
2018-02-06 10:50:47 -0200

    Improvement on db configs

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,14 @@
 'use strict'
 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://user:pass@host:5432/name', {dialect: 'postgres'});
-
+const env = require('./env');
+const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DATABASE_PASSWORD, {
+  host: env.DATABASE_HOST,
+  port: env.DATABASE_PORT,
+  dialect: env.DATABASE_DIALECT,
+  define: {
+    underscored: true
+  }
+});
 // Connect all the models/tables in the database to a db object,
 //so everything is accessible via one object
 const db = {};
