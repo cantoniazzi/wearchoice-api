afd6dce9f00a11572bdeae4b9479c9c94a91e83e
Marciel Torres <marcielribeirotorres@gmail.com>
2018-02-06 14:48:07 -0200

    Create entity Clothing

--- src/config/db.js
+++ src/config/db.js
@@ -19,9 +19,13 @@ db.sequelize = sequelize;
 //Models - tables
 db.user = require('../models/user.js')(sequelize, Sequelize);
 db.userSocialMedia = require('../models/userSocialMedia.js')(sequelize, Sequelize);
+db.clothing = require('../models/clothing.js')(sequelize, Sequelize);
 
 //Relations
 db.userSocialMedia.belongsTo(db.user);
 db.user.hasMany(db.userSocialMedia);
+
+db.clothing.belongsTo(db.user);
+db.user.hasMany(db.clothing);
 
 module.exports = db;
---
+++ src/models/clothing.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+    const Clothing = sequelize.define('Clothing', 
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            uuid: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4
+            },
+            description: {
+                type: DataTypes.STRING(250)
+            },
+            expires_at: {
+                type: DataTypes.DATE,
+                field: 'expires_at'
+            },
+            image: {
+                type: DataTypes.STRING(250)
+            },
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                field: 'user_id'
+            }
+        }, 
+        { 
+            freezeTableName: true,
+            tableName: 'clothing',  
+            timestamps: true,
+            underscored: true
+        }
+    );
+    return Clothing;
+};
