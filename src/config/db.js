da5725c33cd097cbd2e405e0c7cc672b6c63c811
Marciel Torres <marcielribeirotorres@gmail.com>
2018-02-07 15:34:40 -0200

    Improvement on env

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const env = require('./env');
 const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DATABASE_PASSWORD, {
   host: env.DATABASE_HOST,
   port: env.DATABASE_PORT,
-  dialect: env.DATABASE_DIALECT,
+  dialect: env.DATABASE_DIALECT || 'postgre',
   define: {
     underscored: true
   }
